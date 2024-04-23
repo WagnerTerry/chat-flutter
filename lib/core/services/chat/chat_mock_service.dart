5a4aab5a284e0a27a0ce033bff5506fa6bac66e1
Wagner Terry <goncalveswagner15@gmail.com>
2024-04-22 22:18:10 -0300

    chore: ajuste no service do chat

--- lib/core/services/chat/chat_mock_service.dart
+++ lib/core/services/chat/chat_mock_service.dart
@@ -3,8 +3,9 @@ import 'dart:math';
 
 import 'package:chat/core/models/chat_message.dart';
 import 'package:chat/core/models/chat_user.dart';
+import 'package:chat/core/services/chat/chat_service.dart';
 
-class ChatMockService implements ChatMockService {
+class ChatMockService implements ChatService {
   static final List<ChatMessage> _msgs = [
     ChatMessage(
       id: '1',
