38e72f849dbf3d9b44dc25e32cc6516989eda5f7
Wagner Terry <goncalveswagner15@gmail.com>
2024-04-24 13:23:40 -0300

    feat: chat notification service (push notification)

---
+++ lib/core/services/notification/chat_notification_service.dart
@@ -0,0 +1,20 @@
+import 'package:chat/core/models/chat_notification.dart';
+import 'package:flutter/material.dart';
+
+class ChatNotificationService with ChangeNotifier {
+  final List<ChatNotification> _items = [];
+
+  List<ChatNotification> get items {
+    return [...items];
+  }
+
+  void add(ChatNotification notification) {
+    _items.add(notification);
+    notifyListeners();
+  }
+
+  void remove(int i) {
+    _items.removeAt(i);
+    notifyListeners();
+  }
+}
