056fffa2d68a33ca707d65e37c0bcba97ec1daed
Wagner Terry <goncalveswagner15@gmail.com>
2024-05-09 23:02:37 -0300

    chore: iniciando serviço de push notification

--- lib/pages/auth_or_app_page.dart
+++ lib/pages/auth_or_app_page.dart
@@ -1,6 +1,7 @@
 import 'package:chat/constants.dart';
 import 'package:chat/core/models/chat_user.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
+import 'package:chat/core/services/notification/chat_notification_service.dart';
 import 'package:chat/firebase_options.dart';
 import 'package:chat/pages/auth_page.dart';
 import 'package:chat/pages/chat_page.dart';
@@ -8,6 +9,7 @@ import 'package:chat/pages/loading_page.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 class AuthOrAppPage extends StatelessWidget {
   const AuthOrAppPage({super.key});
@@ -37,6 +39,12 @@ class AuthOrAppPage extends StatelessWidget {
       webProvider: ReCaptchaV3Provider('recaptcha-v3-site-key'),
       androidProvider: AndroidProvider.debug,
     );
+
+    // Iniciando chamada Push Notification
+    await Provider.of<ChatNotificationService>(
+      context,
+      listen: false,
+    ).init();
   }
 
   @override
